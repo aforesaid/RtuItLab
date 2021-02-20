ff4fd14e4df857296e5f23275719f8ac752ba2f4
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-02-21 00:07:32 +0300

    Add Purchases.API Extension methods by DTO

---
+++ Purchases.API/Helpers/TransactionContextTransformers.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Purchases.API.Models.ContextModels;
+using Purchases.API.Models.ViewModels;
+
+namespace Purchases.API.Helpers
+{
+    public static class TransactionContextTransformers
+    {
+        public static TransactionContext ToTransactionContext(this Transaction model)
+        {
+            if (model is null)
+                return null;
+            return new TransactionContext
+            {
+                Products        = model.Products.Select(ToProductContext).ToList(),
+                Receipt         = model.Receipt.ToReceiptContext(),
+                Date            = model.Date,
+                TransactionType = model.TransactionType,
+                IsShopCreate    = model.IsShopCreate
+            };
+        }
+
+        public static ProductContext ToProductContext(this Product model)
+        {
+            if (model is null)
+                return null;
+            return new ProductContext
+            {
+                Name      = model.Name,
+                ProductId = model.ProductId,
+                Cost      = model.Cost,
+                Count     = model.Count,
+                Category  = model.Category
+            };
+        }
+
+        public static ReceiptContext ToReceiptContext(this Receipt model)
+        {
+            if (model is null)
+                return null;
+            return new ReceiptContext
+            {
+                ShopId = model.ShopId,
+                Cost   = model.Cost,
+                Count  = model.Count,
+                Date   = model.Date
+            };
+        }
+    }
+}
---
+++ Purchases.API/Helpers/TransactionDTOTransformers.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Purchases.API.Models.ContextModels;
+using Purchases.API.Models.ViewModels;
+
+namespace Purchases.API.Helpers
+{
+    public static class TransactionDtoTransformers
+    {
+        public static Transaction ToTransactionDto (this TransactionContext model)
+        {
+            if (model is null)
+                return null;
+            return new Transaction
+            {
+                Products        = model.Products.Select(ToProductDto).ToList(),
+                Receipt         = ToReceiptDto(model.Receipt),
+                Date            = model.Date,
+                TransactionType = model.TransactionType,
+                IsShopCreate    = model.IsShopCreate
+            };
+        }
+        public static Product ToProductDto(this ProductContext model)
+        {
+            if (model is null)
+                return null;
+            return new Product
+            {
+                Name = model.Name,
+                ProductId = model.ProductId,
+                Cost = model.Cost,
+                Count = model.Count,
+                Category = model.Category
+            };
+        }
+
+        public static Receipt ToReceiptDto(this ReceiptContext model)
+        {
+            if (model is null)
+                return null;
+            return new Receipt
+            {
+                ShopId = model.ShopId,
+                Cost = model.Cost,
+                Count = model.Count,
+                Date = model.Date
+            };
+        }
+    }
+}
