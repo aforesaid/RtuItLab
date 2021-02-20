d8c31ad7e4983b2ec75f4a189ba4d43489f559c2
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-02-21 00:08:01 +0300

    Update Purchases.API PurchasesService

--- Purchases.API/Services/IPurchasesService.cs
+++ Purchases.API/Services/IPurchasesService.cs
@@ -1,16 +1,17 @@
 ﻿using Purchases.API.Models.ViewModels;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Purchases.API.Models.DTOs;
 
 namespace Purchases.API.Services
 {
     public interface IPurchasesService
     {
-        public Task<Transaction> GetTransactionById(int id);
-        public Task<ICollection<Transaction>> GetTransactions();
+        public Task<Transaction> GetTransactionById(UserDTO user, int id);
+        public Task<ICollection<Transaction>> GetTransactions(UserDTO user);
         //TODO: в строке возврщатаь id транзакции
-        public Task<string> AddTransaction(Transaction transaction);
+        public Task AddTransaction(UserDTO user, Transaction transaction);
         //TODO: возвращать false, если пробуют изменить транзакцию магазина (не соблюдение правил)
-        public Task<(string,bool)> UpdateTransaction(UpdateTransaction transaction);
+        public Task<(string,bool)> UpdateTransaction(UserDTO user, UpdateTransaction transaction);
     }
 }
--- Purchases.API/Services/PurchasesService.cs
+++ Purchases.API/Services/PurchasesService.cs
@@ -1,4 +1,14 @@
-﻿using Purchases.API.Data;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Purchases.API.Data;
+using Purchases.API.Models.DTOs;
+using Purchases.API.Models.ViewModels;
+using System.Threading.Tasks;
+using Purchases.API.Helpers;
+using Purchases.API.Models.ContextModels;
 
 namespace Purchases.API.Services
 {
@@ -8,6 +18,64 @@ namespace Purchases.API.Services
         public PurchasesService(PurchasesDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<Transaction> GetTransactionById(UserDTO user, int id)
+        {
+            var customer = await _context.Customers.Include(item => item.Transactions)
+                                                   .ThenInclude(item => item.Products)
+                                                   .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+            var transaction = customer?.Transactions.FirstOrDefault(item => item.Id == id);
+            return transaction.ToTransactionDto();
+        }
+
+        public async Task AddTransaction(UserDTO user, Transaction transaction)
+        {
+            var customer = await _context.Customers.FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+            customer.Transactions.Add(transaction.ToTransactionContext());
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<(string, bool)> UpdateTransaction(UserDTO user, UpdateTransaction transaction)
+        {
+            var customer = await _context.Customers.Include(item => item.Transactions)
+                .ThenInclude(item => item.Products)
+                .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+            var currentTransaction = customer?.Transactions.FirstOrDefault(item => item.Id == transaction.Id);
+            var response = UpdateTransaction(currentTransaction, transaction);
+            return response is null
+                ? (null, true)
+                : (response, false);
+        }
+        public async Task<ICollection<Transaction>> GetTransactions(UserDTO user)
+        {
+            var customer = await _context.Customers.Include(item => item.Transactions)
+                .ThenInclude(item => item.Products)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+            return customer.Transactions.Select(item => item.ToTransactionDto()).ToList();
+        }
+
+        private string UpdateTransaction(TransactionContext transactionContext, UpdateTransaction updateTransaction)
+        {
+            if (transactionContext.IsShopCreate)
+            {
+                if (updateTransaction.Products != null || updateTransaction.Date != new DateTime())
+                    return "You can't change current shop's transaction!";
+                transactionContext.TransactionType = updateTransaction.TransactionType;
+            }
+            else
+                UpdateUserTransaction(transactionContext, updateTransaction);
+            return null;
+
+        }
+        private void UpdateUserTransaction(TransactionContext transactionContext, UpdateTransaction updateTransaction)
+        {
+            transactionContext.TransactionType = updateTransaction.TransactionType;
+            if (updateTransaction.Products != null)
+                transactionContext.Products = updateTransaction.Products.Select(item => item.ToProductContext()).ToList();
+            if (updateTransaction.Date != new DateTime())
+                transactionContext.Date = updateTransaction.Date;
         }
     }
 }
