a3ce1f933041f62bda935cb08eb21b309cf1f7a3
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-02-21 18:51:54 +0300

    Add Shops.API ShopsService

--- Shops.API/Services/IShopsService.cs
+++ Shops.API/Services/IShopsService.cs
@@ -1,6 +1,15 @@
-﻿namespace Shops.API.Services
+﻿using Microsoft.AspNetCore.Mvc;
+using Shops.API.Models.ViewModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shops.API.Services
 {
     public interface IShopsService
     {
+        public ICollection<Shop> GetAllShops();
+        public Task<ICollection<Product>> GetProductsByShop(int shopId);
+        public Task<ICollection<Product>> GetProductsByCategory(int shopId, string categoryName);
+        public Task<string> BuyProducts([FromBody] int shopId, [FromBody] ICollection<Product> products);
     }
 }
--- Shops.API/Services/ShopsService.cs
+++ Shops.API/Services/ShopsService.cs
@@ -1,6 +1,61 @@
-﻿namespace Shops.API.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Shops.API.Data;
+using Shops.API.Helpers;
+using Shops.API.Models.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shops.API.Services
 {
-    public class ShopsService
+    public class ShopsService : IShopsService
     {
+        private const int MaxProductRequestCount = 10;
+        private readonly ShopsDbContext _context;
+        public ShopsService(ShopsDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Shop> GetAllShops()
+        => _context.Shops.Select(item => item.ToShopDto())
+            .AsNoTracking()
+            .ToList();
+
+        public async Task<ICollection<Product>> GetProductsByShop(int shopId)
+        {
+            var shop = await _context.Shops.Include(item => item.Products)
+                .FirstOrDefaultAsync(item => item.Id == shopId);
+            return shop?.Products.Select(item => item.ToProductDto())
+                .ToList();
+        }
+
+        public async Task<ICollection<Product>> GetProductsByCategory(int shopId, string categoryName)
+        {
+            var shop = await _context.Shops.Include(item => item.Products)
+                .FirstOrDefaultAsync(item => item.Id == shopId);
+            return shop?.Products.Where(item => item.Category == categoryName)
+                .Select(item => item.ToProductDto())
+                .ToList();
+        }
+
+        public async Task<string> BuyProducts(int shopId, ICollection<Product> products)
+        {
+            if (products.Count > MaxProductRequestCount)
+                return $"Too many products, max count is {MaxProductRequestCount}";
+            var shop = await _context.Shops.Include(item => item.Products)
+                .FirstOrDefaultAsync(item => item.Id == shopId);
+            var isSuccess = products.Select(product =>
+            {
+                var item = shop.Products.FirstOrDefault(productContext => productContext.Id == product.ProductId);
+                if (item is null || item.Count < product.Count)
+                    return "BadRequest";
+                item.Count -= product.Count;
+                return "Success";
+            }).Any(item => item == "BadRequest");
+            if (!isSuccess) return "No product found";
+            await _context.SaveChangesAsync();
+            return "Success";
+        }
     }
 }
