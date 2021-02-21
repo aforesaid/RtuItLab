a129cb6ac9966e2a23ea35974d20dcb038fa746a
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-02-21 19:30:52 +0300

    Update Shops.API ShopsController

--- Shops.API/Controllers/ShopsController.cs
+++ Shops.API/Controllers/ShopsController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Shops.API.Services;
 using System.Threading.Tasks;
+using Shops.API.Helpers;
+using Shops.API.Models.ViewModel;
 
 namespace Shops.API.Controllers
 {
@@ -11,5 +11,41 @@ namespace Shops.API.Controllers
     [ApiController]
     public class ShopsController : ControllerBase
     {
+        private readonly IShopsService _shopsService;
+
+        public ShopsController(IShopsService shopsService)
+        {
+            _shopsService = shopsService;
+        }
+
+        [HttpGet("shops")]
+        public IActionResult GetAllShops()
+            => Ok(_shopsService.GetAllShops());
+
+        [HttpGet("shops/{id}")]
+        public async Task<IActionResult> GetProducts(int shopId)
+        {
+            var products = await _shopsService.GetProductsByShop(shopId);
+            return Ok(products);
+        }
+
+        [HttpPost("shops/{id]/find_by_category")]
+        public async Task<IActionResult> GetProductsByCategory(int shopId, [FromBody] string categoryName)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var products = await _shopsService.GetProductsByCategory(shopId, categoryName);
+            return Ok(products);
+        }
+
+        [Authorize]
+        [HttpPost("shops/{id}/order")]
+        public async Task<IActionResult> BuyProducts(int shopId, [FromBody] ICollection<Product> products)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var response = await _shopsService.BuyProducts(shopId, products);
+            if (response == "Success")
+                return Ok(response);
+            return BadRequest(response);
+        }
     }
 }
--- Shops.API/Startup.cs
+++ Shops.API/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shops.API.Models.ViewModel;
+using Shops.API.Services;
 
 namespace Shops.API
 {
@@ -18,6 +20,7 @@ namespace Shops.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IShopsService, ShopsService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
