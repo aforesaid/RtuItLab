80497a53e5f59fedad816913dc9edcc889047275
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-03-02 23:31:15 +0300

    Add Purchases.API Add Validation TranscationTypes

--- RtuItLab.Infrastructure/Models/Purchases/UpdateTransaction.cs
+++ RtuItLab.Infrastructure/Models/Purchases/UpdateTransaction.cs
@@ -11,6 +11,17 @@ namespace RtuItLab.Infrastructure.Models.Purchases
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public List<Product> Products { get; set; }
+        [RequireTransactionType]
         public TransactionTypes TransactionType { get; set; }
+    }
+    public class RequireTransactionType : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var employee = (int)value;
+            if (employee == 0 || employee == 1)
+                return ValidationResult.Success;
+            return new ValidationResult("Invalid Transaction Type");
+        }
     }
 }
