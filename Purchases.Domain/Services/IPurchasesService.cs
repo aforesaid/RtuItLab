e52346610524d161a62437f399debc3b4cff9b19
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-03-04 23:14:15 +0300

    Refactoring Purchases.Domain Change Response by ResponseMassTransit

--- Purchases.Domain/Services/IPurchasesService.cs
+++ Purchases.Domain/Services/IPurchasesService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using RtuItLab.Infrastructure.MassTransit;
 using RtuItLab.Infrastructure.Models.Identity;
 using RtuItLab.Infrastructure.Models.Purchases;
 
@@ -7,9 +8,9 @@ namespace Purchases.Domain.Services
 {
     public interface IPurchasesService
     {
-        public Task<Transaction> GetTransactionById(User user, int id);
-        public Task<ICollection<Transaction>> GetTransactions(User user);
-        public Task AddTransaction(User user, Transaction transaction);
-        public Task<(string,bool)> UpdateTransaction(User user, UpdateTransaction transaction);
+        public Task<ResponseMassTransit<Transaction>> GetTransactionById(User user, int id);
+        public Task<ResponseMassTransit<ICollection<Transaction>>> GetTransactions(User user);
+        public Task<ResponseMassTransit<BaseResponseMassTransit>> AddTransaction(User user, Transaction transaction);
+        public Task<ResponseMassTransit<BaseResponseMassTransit>> UpdateTransaction(User user, UpdateTransaction transaction);
     }
 }
--- Purchases.Domain/Services/PurchasesService.cs
+++ Purchases.Domain/Services/PurchasesService.cs
@@ -1,64 +1,87 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Purchases.DAL.ContextModels;
-using Purchases.DAL.Data;
-using Purchases.Domain.Helpers;
-using RtuItLab.Infrastructure.Models.Purchases;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Purchases.DAL.ContextModels;
+using Purchases.DAL.Data;
+using Purchases.Domain.Helpers;
+using RtuItLab.Infrastructure.Exceptions;
+using RtuItLab.Infrastructure.MassTransit;
 using RtuItLab.Infrastructure.Models.Identity;
+using RtuItLab.Infrastructure.Models.Purchases;
 
 namespace Purchases.Domain.Services
 {
     public class PurchasesService : IPurchasesService
     {
-        private readonly PurchasesDbContext _context; 
+        private readonly PurchasesDbContext _context;
+
         public PurchasesService(PurchasesDbContext context)
         {
             _context = context;
         }
 
-        public async Task<Transaction> GetTransactionById(User user, int id)
+        public async Task<ResponseMassTransit<Transaction>> GetTransactionById(User user, int id)
         {
             await CheckUserIsCreate(user);
-            var customer = await _context.Customers.Include(item => item.Transactions)
-                                                   .ThenInclude(item => item.Products)
-                                                   .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
-            var transaction = customer?.Transactions.FirstOrDefault(item => item.Id == id);
-            return transaction.ToTransactionDto();
-        }
-
-        public async Task AddTransaction(User user, Transaction transaction)
-        {
-            await CheckUserIsCreate(user);
-            var customer = await _context.Customers.FirstOrDefaultAsync(item => item.CustomerId == user.Id);
-            customer.Transactions.Add(transaction.ToTransactionContext());
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<(string, bool)> UpdateTransaction(User user, UpdateTransaction transaction)
-        {
-            await CheckUserIsCreate(user);
+            var response = new ResponseMassTransit<Transaction>();
             var customer = await _context.Customers.Include(item => item.Transactions)
                 .ThenInclude(item => item.Products)
                 .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
-            var currentTransaction = customer?.Transactions.FirstOrDefault(item => item.Id == transaction.Id);
-            if (currentTransaction is null)
-                return ("Invalid Transaction", false);
-            var response = await UpdateTransaction(currentTransaction, transaction);
-            return response is null
-                ? (null, true)
-                : (response, false);
+            var transaction = customer?.Transactions.FirstOrDefault(item => item.Id == id);
+            if (transaction is null)
+                response.Exception = new NotFoundException("Transaction not found!");
+            else
+                response.Content = transaction.ToTransactionDto();
+            return response;
         }
-        public async Task<ICollection<Transaction>> GetTransactions(User user)
+
+        public async Task<ResponseMassTransit<BaseResponseMassTransit>> AddTransaction(User user,
+            Transaction transaction)
         {
             await CheckUserIsCreate(user);
+            var response = new ResponseMassTransit<BaseResponseMassTransit>();
+            var customer = await _context.Customers.FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+            customer.Transactions.Add(transaction.ToTransactionContext());
+            await _context.SaveChangesAsync();
+            response.Content = new BaseResponseMassTransit();
+            return response;
+        }
+
+        public async Task<ResponseMassTransit<BaseResponseMassTransit>> UpdateTransaction(User user,
+            UpdateTransaction transaction)
+        {
+            await CheckUserIsCreate(user);
+            var response = new ResponseMassTransit<BaseResponseMassTransit>();
+            try
+            {
+                var customer = await _context.Customers.Include(item => item.Transactions)
+                    .ThenInclude(item => item.Products)
+                    .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
+                var currentTransaction = customer?.Transactions.FirstOrDefault(item => item.Id == transaction.Id);
+                if (currentTransaction is null)
+                    throw new NotFoundException("Transaction that is being updated was not found"); 
+                await UpdateTransaction(currentTransaction, transaction);
+                response.Content = new BaseResponseMassTransit();
+            }
+            catch (Exception e)
+            {
+                response.Exception = e;
+            }
+            return response;
+        }
+
+        public async Task<ResponseMassTransit<ICollection<Transaction>>> GetTransactions(User user)
+        {
+            await CheckUserIsCreate(user);
+            var response = new ResponseMassTransit<ICollection<Transaction>>();
             var customer = await _context.Customers.Include(item => item.Transactions)
                 .ThenInclude(item => item.Receipt)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(item => item.CustomerId == user.Id);
-            return customer.Transactions.Select(item => item.ToTransactionDto()).ToList();
+            response.Content = customer.Transactions.Select(item => item.ToTransactionDto()).ToList();
+            return response;
         }
 
         private async Task CheckUserIsCreate(User user)
@@ -74,24 +97,27 @@ namespace Purchases.Domain.Services
             }
         }
 
-        private async Task<string> UpdateTransaction(TransactionContext transactionContext, UpdateTransaction updateTransaction)
+        private async Task UpdateTransaction(TransactionContext transactionContext,
+            UpdateTransaction updateTransaction)
         {
             if (transactionContext.IsShopCreate)
             {
                 if (updateTransaction.Products != null || updateTransaction.Date != new DateTime())
-                    return "You can't change current shop's transaction!";
+                    throw new BadRequestException("You can't change current shop's transaction!");
                 transactionContext.TransactionType = updateTransaction.TransactionType;
                 await _context.SaveChangesAsync();
             }
             else
                 await UpdateUserTransaction(transactionContext, updateTransaction);
-            return null;
         }
-        private async Task UpdateUserTransaction(TransactionContext transactionContext, UpdateTransaction updateTransaction)
+
+        private async Task UpdateUserTransaction(TransactionContext transactionContext,
+            UpdateTransaction updateTransaction)
         {
             transactionContext.TransactionType = updateTransaction.TransactionType;
             if (updateTransaction.Products != null)
-                transactionContext.Products = updateTransaction.Products.Select(item => item.ToProductContext()).ToList();
+                transactionContext.Products =
+                    updateTransaction.Products.Select(item => item.ToProductContext()).ToList();
             if (updateTransaction.Date != new DateTime())
                 transactionContext.Date = updateTransaction.Date;
             await _context.SaveChangesAsync();
