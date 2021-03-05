90c5bc038dd8673eddd1a30f39579a04649f6bd6
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-03-05 20:12:29 +0300

    Refactoring RabbitMQ Add ConfigureRabbitMQ

--- RabbitMQ/Startup.cs
+++ RabbitMQ/Startup.cs
@@ -1,10 +1,8 @@
 using Factories.DAL.Data;
 using Factories.Domain.Services;
-using GreenPipes;
 using Identity.DAL.ContextModels;
 using Identity.DAL.Data;
 using Identity.Domain.Services;
-using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,13 +11,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
 using Purchases.DAL.Data;
 using Purchases.Domain.Services;
-using RabbitMQ.Consumers.Factories;
-using RabbitMQ.Consumers.Identity;
-using RabbitMQ.Consumers.Purchases;
-using RabbitMQ.Consumers.Shops;
 using Shops.DAL.Data;
 using Shops.Domain.Services;
 
@@ -53,81 +46,7 @@ namespace RabbitMQ
             services.AddScoped<IShopsService, ShopsService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IFactoriesService, FactoriesService>();
-            services.AddMassTransit(x =>
-            {
-                // Identity
-                x.AddConsumer<Authenticate>();
-                x.AddConsumer<CreateUser>();
-                x.AddConsumer<GetUserByToken>();
-
-
-                // Purchases
-                x.AddConsumer<AddTransaction>();
-                x.AddConsumer<GetTransactionById>();
-                x.AddConsumer<GetTransactions>();
-                x.AddConsumer<UpdateTransaction>();
-
-                // Shops
-                x.AddConsumer<BuyProducts>();
-                x.AddConsumer<GetAllShops>();
-                x.AddConsumer<GetProductsByCategory>();
-                x.AddConsumer<GetProductsByShop>();
-
-                // Factories
-                x.AddConsumer<SubmitOrderByAddProducts>();
-
-                x.UsingRabbitMq((context, cfg) =>
-                    {
-                        cfg.Host("rabbitmq://guest:guest@host.docker.internal:5672");
-
-                        cfg.ReceiveEndpoint("identityQueue", e =>
-                        {
-                            e.PrefetchCount = 20;
-                            e.UseMessageRetry(r => r.Interval(2, 100));
-
-                            //// Identity
-                            e.Consumer<Authenticate>(context);
-                            e.Consumer<CreateUser>(context);
-                            e.Consumer<GetUserByToken>(context);
-
-                        });
-                        cfg.ReceiveEndpoint("purchasesQueue", e =>
-                        {
-                            e.PrefetchCount = 20;
-                            e.UseMessageRetry(r => r.Interval(2, 100));
-
-                            // Purchases
-                            e.Consumer<AddTransaction>(context);
-                            e.Consumer<GetTransactionById>(context);
-                            e.Consumer<GetTransactions>(context);
-                            e.Consumer<UpdateTransaction>(context);
-                        });
-                        cfg.ReceiveEndpoint("shopsQueue", e =>
-                        {
-                            e.PrefetchCount = 20;
-                            e.UseMessageRetry(r => r.Interval(2, 100));
-
-                            // Shops
-                            e.Consumer<BuyProducts>(context);
-                            e.Consumer<GetAllShops>(context);
-                            e.Consumer<GetProductsByCategory>(context);
-                            e.Consumer<GetProductsByShop>(context);
-                        });
-                        cfg.ReceiveEndpoint("factoriesQueue", e =>
-                        {
-                            e.PrefetchCount = 20;
-                            e.UseMessageRetry(r => r.Interval(2, 100));
-
-                            // Factories Consumer by Shops
-                            e.Consumer<SubmitOrderByAddProducts>(context);
-                        });
-                        cfg.ConfigureJsonSerializer(settings =>
-                        {
-                            return settings;
-                        });
-                    });
-            });
-            services.AddMassTransitHostedService();
+            services.AddMassTransitConfigure();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
