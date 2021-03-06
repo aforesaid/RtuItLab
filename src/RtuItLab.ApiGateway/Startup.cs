677ca5583475396a3732f6afdd6d072e2a9267e5
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-03-06 17:54:30 +0300

    Refactoring ApiGateway

--- src/RtuItLab.ApiGateway/Startup.cs
+++ src/RtuItLab.ApiGateway/Startup.cs
@@ -21,52 +21,11 @@ namespace RtuItLab.ApiGateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo()
-                {
-                    Title = "Identity Service",
-                    Version = "v1"
-                });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = @"JWT Authorization header using the Bearer scheme.
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      Example: 'Bearer 12345abcdef'",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id   = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name   = "Bearer",
-                            In     = ParameterLocation.Header,
-
-                        },
-                        new List<string>()
-                    }
-                });
-            });
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
            
             app.UseOcelot().Wait();
-            app.UseSwagger()
-                .UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity.API V1");
-                });
         }
     }
 }
