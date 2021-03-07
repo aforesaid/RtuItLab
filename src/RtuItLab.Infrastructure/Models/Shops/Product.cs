6c5ab22330c0c9642eb31e3245c28e95396e9da7
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-03-08 02:51:19 +0300

    Add ValidationAttribute of Product

--- src/RtuItLab.Infrastructure/Models/Shops/Product.cs
+++ src/RtuItLab.Infrastructure/Models/Shops/Product.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using RtuItLab.Infrastructure.Exceptions;
+using System.ComponentModel.DataAnnotations;
 
 namespace RtuItLab.Infrastructure.Models.Shops
 {
+    [ProductValidation]
     public class Product
     {
         public string Name { get; set; }
@@ -9,5 +11,17 @@ namespace RtuItLab.Infrastructure.Models.Shops
         public decimal Cost { get; set; }
         public int Count { get; set; }
         public string Category { get; set; }
+    }
+    public class ProductValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var employee = (Product)value;
+            if (employee.ProductId<1)
+                throw new BadRequestException($"Invalid ProductId: ProductId can't be {employee.ProductId}");
+            if (employee.Count<1)
+                throw new BadRequestException($"Invalid Count: Product count can't be {employee.ProductId}");
+            return ValidationResult.Success;
+        }
     }
 }
